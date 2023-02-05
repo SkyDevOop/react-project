70c1cf6a23f5ce17d231607d032ee885a3f92aa2
issam bourouail <issambourouail9@gmail.com>
2023-02-05 05:10:34 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+
+function App() {
+  return (
+  <>
+      <h1 className="title">My First Project With React</h1>
+      <h2>My First Project With React</h2>
+  </>
+  );
+}
+
+export default App;
